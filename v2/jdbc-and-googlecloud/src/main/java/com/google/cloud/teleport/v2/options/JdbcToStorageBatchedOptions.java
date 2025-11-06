e15aeca5ca4ce4efcec2031568e17dc84d229e49
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2025-11-06 17:33:21 -0300

    Fix formatting and wrong function call

--- v2/jdbc-and-googlecloud/src/main/java/com/google/cloud/teleport/v2/options/JdbcToStorageBatchedOptions.java
+++ v2/jdbc-and-googlecloud/src/main/java/com/google/cloud/teleport/v2/options/JdbcToStorageBatchedOptions.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
-package com.google.cloud.teleport.v2.templates;
+package com.google.cloud.teleport.v2.options;
 
 import com.google.cloud.teleport.v2.options.JdbcToStorageOptions;
 import org.apache.beam.sdk.options.Description;
@@ -12,30 +12,31 @@ import org.apache.beam.sdk.options.Validation;
 /**
  * Extra options for the Jdbc_to_Storage_Batched template.
  *
- * In addition to JdbcToStorageOptions (driverJars, driverClassName,
- * connectionUrl, username, password, KMSEncryptionKey, etc.), this template
- * accepts:
+ * Inherits all JdbcToStorageOptions (driverJars, driverClassName, connectionUrl,
+ * username, password, KMSEncryptionKey, etc.) and adds:
  *
- * - tableConfigs: JSON array of table configs
- * - maxParallelTables: advisory concurrency hint
+ * - tableConfigs: JSON array of table configs.
+ * - maxParallelTables: advisory concurrency hint.
  */
 public interface JdbcToStorageBatchedOptions extends JdbcToStorageOptions {
 
-    @Description("JSON array of table configs. Example:\n"
-            + "[\n"
-            + "  {\"table_name\":\"Pedido_PedidoStatus\",\n"
-            + "   \"query\":\"SELECT * FROM Pedido_PedidoStatus WHERE DataInsercao >= '2025-01-01'\",\n"
-            + "   \"output_directory\":\"gs://bucket/RAW/Pedido_PedidoStatus/...\",\n"
-            + "   \"output_filename_prefix\":\"/part-\"}\n"
-            + "]")
-    @Validation.Required
-    String getTableConfigs();
+  @Description(
+      "JSON array of table configs. Example:\n"
+          + "[\n"
+          + "  {\"table_name\":\"Pedido_PedidoStatus\",\n"
+          + "   \"query\":\"SELECT * FROM Pedido_PedidoStatus WHERE DataInsercao >= '2025-01-01'\",\n"
+          + "   \"output_directory\":\"gs://bucket/RAW/Pedido_PedidoStatus/...\",\n"
+          + "   \"output_filename_prefix\":\"/part-\"}\n"
+          + "]")
+  @Validation.Required
+  String getTableConfigs();
 
-    void setTableConfigs(String value);
+  void setTableConfigs(String value);
 
-    @Description("Max number of tables to process in parallel (advisory only). "
-            + "Used to size worker pool. Default 8.")
-    String getMaxParallelTables();
+  @Description(
+      "Max number of tables to process in parallel (advisory only). "
+          + "Used to size worker pool. Default 8.")
+  String getMaxParallelTables();
 
-    void setMaxParallelTables(String value);
+  void setMaxParallelTables(String value);
 }
